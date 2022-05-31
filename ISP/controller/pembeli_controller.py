a71ec13c937f6767265d16ba64e95e6534d2f505
farhanahazh <farhanahzhr18@gmail.com>
2022-05-31 15:08:20 +0700

    punya farhanah

---
+++ ISP/controller/pembeli_controller.py
@@ -0,0 +1,6 @@
+from interface.pembeli_operation import PembeliOperation
+
+class PembeliController(PembeliController):
+    
+    def memesan_pesanan(self):
+        print("Pembeli melakuksan pesanan")
