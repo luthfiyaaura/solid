5c535e5e4ef815e3bb5e7be63bf2bec9226420b9
luthfiyaaura <luthfiyaaura05@gmail.com>
2022-05-31 14:48:48 +0700

    Aura

---
+++ OCP/main.py
@@ -0,0 +1,12 @@
+from medium_product import MediumProduct
+from large_product import LargeProduct
+from small_product import SmallProduct
+
+small = SmallProduct(5000, "Popcorn")
+print(small.calculate_delivery())
+
+medium = MediumProduct(6000, "Popcorn")
+print(medium.calculate_delivery())
+
+large = LargeProduct(7000, "Popcorn")
+print(large.calculate_delivery())
